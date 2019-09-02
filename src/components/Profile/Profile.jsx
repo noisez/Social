8e78d80f39fec9eb2771c37d930f275f07bac8f7
noisez <onceagain@ua.fm>
2019-09-02 17:24:02 +0300

    local state

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ProfileInfo from './ProfileInfo/ProfileInfo'
 import './profile.css'
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import ProfileStatus from "./ProfileInfo/ProfileStatus";
 
 const Profile = (props) => {
     return (
         <div className="profile">
-            <div className="profile__header">
-                <img src="https://marin.ru/ai/1000/geo.113.thumb.big/fiji-main(pics.1).jpg" alt="icon" className="header__img"/>
-            </div>
+            {/*<div className="profile__header">*/}
+                {/*<img src="https://marin.ru/ai/1000/geo.113.thumb.big/fiji-main(pics.1).jpg" alt="icon" className="header__img"/>*/}
+            {/*</div>*/}
             <ProfileInfo profile={props.profile} />
+            <ProfileStatus status={"This is my status"} />
             <MyPostsContainer />
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class ProfileStatus extends React.Component {
+
+    state = {
+        editMode: false,
+        title: 'yo'
+    };
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        });
+    };
+
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        });
+    };
+
+    render() {
+        return (
+            <div className="profile__status">
+                {this.state.editMode ? (<input type="text" autoFocus={true} onBlur={this.deactivateEditMode} className="status__input" value={this.props.status}/>)
+                    : (<p onDoubleClick={this.activateEditMode} className="status__text">{this.props.status}</p>)}
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
